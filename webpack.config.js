8bb430a8c9f85fb34a5384b31ac04446bb00eecc
Alberto N. 🦑 <alberton@spotify.com>
2020-12-12 18:19:23 -0500

    basic skeleton part 1

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    background:'./src/background.ts',
+    content: './src/content.ts',
+    popup: `./src/popup.ts`
+  },
+devtool: 'inline-source-map',
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  }
+};
